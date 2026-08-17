4a4f1eb3ce6724f75960088e91d2728f265fee78
Hirotaka <hirotaka1@keio.jp>
2026-08-17 21:05:05 +0900

    feat: マイページ大枠実装

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,11 +1,7 @@
-import { Placeholder } from "@/components/Placeholder";
+"use client";
+
+import { MyPage } from "@/components/profile/MyPage";
 
 export default function MePage() {
-  return (
-    <Placeholder
-      title="マイページ"
-      branch="feat/profile"
-      note="自分のプロフィール編集。仕事モードと恋愛モードで別々の内容を持てます。"
-    />
-  );
+  return <MyPage />;
 }
